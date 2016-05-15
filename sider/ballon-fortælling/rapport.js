e09f100a6ec2fc5dd6939167f25356d9238894ab
LudvigSoerensen <ludvigsoerensen@gmail.com>
2016-05-15 15:23:35 +0200

    ballon: update til sass, html, js

--- sider/ballon-fortælling/rapport.js
+++ sider/ballon-fortælling/rapport.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   clickImage();
+  sassOrCSS();
 });
 
 var imageIsZoomed = false;
@@ -13,5 +14,23 @@ function clickImage() {
       $(this).removeClass('show-image');
       imageIsZoomed = false;
     }
+  });
+}
+
+
+function sassOrCSS() {
+  function sassTocssToggle() {
+    $('.sass').toggleClass('is-gone');
+    $('.css').toggleClass('is-gone');
+    $('.sass-btn').toggleClass('sass-css-btn-is-on');
+    $('.css-btn').toggleClass('sass-css-btn-is-on');
+  }
+
+  $('.css-btn').click(function() {
+    sassTocssToggle();
+  });
+  
+  $('.sass-btn').click(function() {
+    sassTocssToggle();
   });
 }
